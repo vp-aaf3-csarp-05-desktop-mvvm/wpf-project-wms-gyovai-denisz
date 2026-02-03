dfd39964bf42a78a605ffc2204ae0501046dc7be
c1gyoden <c1gyoden@vasvari.org>
2026-02-03 10:38:06 +0100

    adószám és szabadnapok száma hozzáadása az Employee-nak, illetve megjelenítése

--- WMS_Project/EmployeeViewModel.cs
+++ WMS_Project/EmployeeViewModel.cs
@@ -2,6 +2,8 @@
 {
     public class EmployeeViewModel
     {
-        public string Name { get; } = "Nagy Anna";
+        public string Name { get; } = "Nagy Anna";  // {get; }: más osztályok lekérhetik
+        public string Adoszam { get; } = "1234567890";
+        public int Szabadnapok { get; } = 10;
     }
 }
